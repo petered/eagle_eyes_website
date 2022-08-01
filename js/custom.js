82e31d76d51d9454f58a86e831e303b3ea027d8b
peter <peter.ed.oconnor@gmail.com>
2022-08-01 11:05:20 -0600

    ok, try that

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,11 @@ $('#siteNav').affix({
 var backgroundInterval = setInterval(function(){
     $(".blinky").toggleClass("backgroundRed");
  },300)
+
+$("#slider").on("input change", (e)=>{
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $('.foreground-img').css('width', `${sliderPos}%`)
+  // Update the position of the slider button
+  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+});
